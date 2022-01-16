913ca4847ce72e47034ed9ba29e359f9bdc8d1d3
Olvine23 <olivinegeorge@gmal.com>
2022-01-16 12:48:08 -0800

    image adjustments

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,9 +1,13 @@
+import iat from "../images/organizer/iat.png";
+import kncc from "../images/organizer/kncc.jpeg"
+import county from "../images/organizer/cc.jpeg"
+import monte from '../images/organizer/monte.jpeg'
 export const Team = () => {
     return (
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="mx-auto mb-10 lg:max-w-xl sm:text-center">
           <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-            Lorem Team
+            Event Organizers
           </p>
           <p className="text-base text-gray-700 md:text-lg">
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem
@@ -13,8 +17,8 @@ export const Team = () => {
         <div className="grid gap-10 mx-auto lg:max-w-screen-lg sm:grid-cols-2 lg:grid-cols-4">
           <div className="flex flex-col items-center">
             <img
-              className="object-cover w-20 h-20 mb-2 rounded-full shadow"
-              src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+              className="object-cover w-auto h-20 mb-2   shadow"
+              src={iat}
               alt="Person"
             />
             <div className="flex flex-col items-center">
@@ -24,8 +28,8 @@ export const Team = () => {
           </div>
           <div className="flex flex-col items-center">
             <img
-              className="object-cover w-20 h-20 mb-2 rounded-full shadow"
-              src="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+              className="object-cover w-auto h-20 mb-2  shadow"
+              src= {county}
               alt="Person"
             />
             <div className="flex flex-col items-center">
@@ -35,8 +39,8 @@ export const Team = () => {
           </div>
           <div className="flex flex-col items-center">
             <img
-              className="object-cover w-20 h-20 mb-2 rounded-full shadow"
-              src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+              className="object-cover w-auto h-20 mb-2  shadow"
+              src= {kncc}
               alt="Person"
             />
             <div className="flex flex-col items-center">
@@ -46,8 +50,8 @@ export const Team = () => {
           </div>
           <div className="flex flex-col items-center">
             <img
-              className="object-cover w-20 h-20 mb-2 rounded-full shadow"
-              src="https://images.pexels.com/photos/3747435/pexels-photo-3747435.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+              className="object-cover w-auto  h-20 mb-2 "
+              src= {monte}
               alt="Person"
             />
             <div className="flex flex-col items-center">
